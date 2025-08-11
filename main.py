da9dd69cc52b47a093e499883b46b3dc90d07a6a
Michael Hammond <33749130+mickeymond@users.noreply.github.com>
2025-08-11 11:56:59 +0000

    Added pseudocode

---
+++ main.py
@@ -0,0 +1,4 @@
+# Ask user to input the length of the 3 sides of a triangle
+# If all sides are equal print Equilateral
+# If 2 sides are equal print Isosceles
+# If no sides are equal print Scalene
