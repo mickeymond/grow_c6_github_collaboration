83778a1695678f6b40b9ed055fd9717277c9f10e
Py-Me23 <oforiafia1@gmail.com>
2025-08-11 12:05:56 +0000

    collecting user input

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Ask user to input the length of the 3 sides of a triangle
+x=int(input("Enter first side:\n"))
+y=int(input("Enter second side:\n"))
+z=int(input("Enter third side:\n"))
 # If all sides are equal print Equilateral
 # If 2 sides are equal print Isosceles
 # If no sides are equal print Scalene
