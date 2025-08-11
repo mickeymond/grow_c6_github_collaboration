9e41a9ecc2fb4b851254800bdff4b93dab20d83b
KKB_Official <kkboahene003@gmail.com>
2025-08-11 12:12:31 -0700

    Equilateral changes

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ x=int(input("Enter first side:\n"))
 y=int(input("Enter second side:\n"))
 z=int(input("Enter third side:\n"))
 # If all sides are equal print Equilateral
+if x == y and y == z:
+    print("Equilateral")
 # If 2 sides are equal print Isosceles
 # If no sides are equal print Scalene
